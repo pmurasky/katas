a9e7a8fdb2fcf424bbe933d0da3a88b98ffd1bf4
Peter Murasky <peter.murasky@gmail.com>
2015-07-07 16:13:02 -0400

    Rename score member variables name

--- Tennis-Refactoring-Kata/java/src/main/java/murasky/tennis/TennisGame1.java
+++ Tennis-Refactoring-Kata/java/src/main/java/murasky/tennis/TennisGame1.java
@@ -2,8 +2,8 @@ package murasky.tennis;
 
 public class TennisGame1 implements TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+    private int score1 = 0;
+    private int score2 = 0;
     private String player1Name;
     private String player2Name;
 
@@ -14,17 +14,17 @@ public class TennisGame1 implements TennisGame {
 
     public void wonPoint(String playerName) {
         if (playerName == "player1")
-            m_score1 += 1;
+            score1 += 1;
         else
-            m_score2 += 1;
+            score2 += 1;
     }
 
     public String getScore() {
         String score = "";
         int tempScore=0;
-        if (m_score1==m_score2)
+        if (score1==score2)
         {
-            switch (m_score1)
+            switch (score1)
             {
                 case 0:
                         score = "Love-All";
@@ -41,9 +41,9 @@ public class TennisGame1 implements TennisGame {
                 
             }
         }
-        else if (m_score1>=4 || m_score2>=4)
+        else if (score1>=4 || score2>=4)
         {
-            int minusResult = m_score1-m_score2;
+            int minusResult = score1-score2;
             if (minusResult==1) score ="Advantage player1";
             else if (minusResult ==-1) score ="Advantage player2";
             else if (minusResult>=2) score = "Win for player1";
@@ -53,8 +53,8 @@ public class TennisGame1 implements TennisGame {
         {
             for (int i=1; i<3; i++)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
+                if (i==1) tempScore = score1;
+                else { score+="-"; tempScore = score2;}
                 switch(tempScore)
                 {
                     case 0:
